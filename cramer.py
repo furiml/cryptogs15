87778ff46ac63e783c3b7d36498ef41aef108527
Furiml <a.screve.94801@gmail.com>
2018-01-01 00:12:59 +0100

    letting the user choose a prime. Prime test not implemented yet. Some commenting added.

--- cramer.py
+++ cramer.py
@@ -3,13 +3,14 @@
 import random
 
 Errors = (ValueError, NameError)
+# error handling easier using a tuple later on
 
 class Public:
 	def __init__(self):
 		global Errors
 		return
 
-	def create_public(self,input_prime):
+	def create_public(self):
 		while True :
 			try :
 				input_prime = int(input('Choose a prime : '))
@@ -17,6 +18,7 @@ class Public:
 				print 'Your choice isn\'t a prime'	# TODO 
 			else :
 				break
+		# generators listing
 		prime_generators = []
 		for i in range (2,input_prime):
 			for j in range (1,input_prime):
@@ -25,6 +27,7 @@ class Public:
 						prime_generators.append(i)
 					else :
 						break
+		# variables definitions
 		while True :
 			prime_generators_dummy = prime_generators
 			try :
@@ -38,19 +41,21 @@ class Public:
 				continue
 			else : 
 				break
-		while True :
-			if len(prime_generators_dummy) == 1 :
-				alpha2 = prime_generators_dummy[0]
-			else :
-				try :
-					alpha2 = int(input('Choose your second generator amongst {} : '.format(prime_generators_dummy)))
-					if alpha2 not in prime_generators_dummy :
-						raise ValueError
-				except Errors :
-					print 'Your choice wasn\'t in the generator list. Try again.'
-					continue
-				else :
-					break
+		if len(prime_generators_dummy) == 1 : # after we remove the chosen generator, if there's only one remaining, choose it
+			print 'Only one generator left. Automatically choosing {} as second generator.'.format(prime_generators_dummy[0])
+			alpha2 = prime_generators_dummy[0]
+		else :
+			while True :
+					try :
+						alpha2 = int(input('Choose your second generator amongst {} : '.format(prime_generators_dummy)))
+						if alpha2 not in prime_generators_dummy :
+							raise ValueError
+					except Errors :
+						print 'Your choice wasn\'t in the generator list. Try again.'
+						continue
+					else :
+						break
+		# now we choose the integers x1, x2, y1, y2, w
 		while True :
 			try :
 				x1 = input('Choose an integer (x1) smaller than {} : '.format(input_prime))
@@ -101,6 +106,7 @@ class Public:
 				continue
 			else :
 				break
+		# calculate x, y, and w
 		x = (alpha1**x1)*(alpha2**x2)
 		y = (alpha1**y1)*(alpha2**y2)
 		w = alpha1**w_input
@@ -108,4 +114,4 @@ class Public:
 
 
 my_public = Public()
-print my_public.create_public(23)
+print my_public.create_public()
