d3ccfb2f8efd9b447dbc996ff33fd0690ad1071d
Furiml <a.screve.94801@gmail.com>
2017-12-31 23:49:16 +0100

    corrected selection of integers, primes and everything. Now returns everything needed for the public key

--- cramer.py
+++ cramer.py
@@ -2,8 +2,11 @@
 
 import random
 
+Errors = (ValueError, NameError)
+
 class Public:
 	def __init__(self):
+		global Errors
 		return
 
 	def create_public(self,input_prime):
@@ -15,16 +18,87 @@ class Public:
 						prime_generators.append(i)
 					else :
 						break
-		while True:
+		while True :
 			prime_generators_dummy = prime_generators
-			alpha1 = input('Choose your first generator amongst {} : '.format(prime_generators_dummy))
-			if alpha1 in prime_generators_dummy :
-				prime_generators_dummy.pop(index(alpha1))
+			try :
+				alpha1 = input('Choose your first generator amongst {} : '.format(prime_generators_dummy))
+				if alpha1 in prime_generators_dummy :
+					prime_generators_dummy.remove(alpha1)
+				else :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t in the generator list. Try again.'
+				continue
+			else : 
+				break
+		while True :
+			if len(prime_generators_dummy) == 1 :
+				alpha2 = prime_generators_dummy[0]
 			else :
-				raise ValueError
-			alpha2 = input('Choose your second generator amongst {} : ').format(prime_generators_dummy)
-			if alpha2 not in prime_generators_dummy :
-				raise ValueError
+				try :
+					alpha2 = int(input('Choose your second generator amongst {} : '.format(prime_generators_dummy)))
+					if alpha2 not in prime_generators_dummy :
+						raise ValueError
+				except Errors :
+					print 'Your choice wasn\'t in the generator list. Try again.'
+					continue
+				else :
+					break
+		while True :
+			try :
+				x1 = input('Choose an integer (x1) smaller than {} : '.format(input_prime))
+				if x1 > input_prime :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t smaller than {}. Try again'.format(input_prime)
+				continue
+			else :
+				break
+		while True :
+			try :
+				x2 = input('Choose a second integer (x2) smaller than {} : '.format(input_prime))
+				if x2 > input_prime :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t smaller than {}. Try again'.format(input_prime)
+				continue
+			else : 
+				break
+		while True :
+			try :
+				y1 = input('Choose a third integer (y1) smaller than {} : '.format(input_prime))
+				if y1 > input_prime :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t smaller than {}. Try again'.format(input_prime)
+				continue
+			else : 
+				break
+		while True :
+			try :
+				y2 = input('Choose a fourth integer (y2) smaller than {} : '.format(input_prime))
+				if y2 > input_prime :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t smaller than {}. Try again'.format(input_prime)
+				continue
+			else : 
+				break
+		while True :
+			try :
+				w_input = input('Choose a last integer (w) smaller than {} : '.format(input_prime))
+				if w_input > input_prime :
+					raise ValueError
+			except Errors :
+				print 'Your choice wasn\'t smaller than {}. Try again'.format(input_prime)
+				continue
+			else :
+				break
+		x = (alpha1**x1)*(alpha2**x2)
+		y = (alpha1**y1)*(alpha2**y2)
+		w = alpha1**w_input
+		return (input_prime, alpha1, alpha2, x, y, w)
+
 
 my_public = Public()
-print my_public.create_public(7)
+print my_public.create_public(23)
